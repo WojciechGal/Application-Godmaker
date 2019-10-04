0d5d4e91594e5c2c7b79a13547f77386e3266917
Wojciech <wojciech.gladalski@gmail.com>
2019-10-04 20:47:09 +0200

    updates

--- src/main/java/pl/wojciech/appgodmaker/login/LoginController.java
+++ src/main/java/pl/wojciech/appgodmaker/login/LoginController.java
@@ -1,11 +1,7 @@
 package pl.wojciech.appgodmaker.login;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
